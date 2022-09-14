8dcd0c68f8244919f86e5ef8f91f8dee0631d7e3
PVG Develop <vargan.pg@gmail.com>
2022-09-13 23:28:32 -0400

    changed pokedex buttons and functions to fetch data based on region

--- public/script.js
+++ public/script.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   const fetchBtn = document.getElementById('fetch-btn');
   const responseText = document.getElementById('response-output');
-  const fetchPokedexBtn = document.getElementById('fetch-pokedex-btn');
+
+  const fetchKantoBtn = document.getElementById('fetch-kanto-btn');
+  const fetchHoennBtn = document.getElementById('fetch-hoenn-btn');
 
   fetchBtn.addEventListener('click', async () => {
     const response = await fetch('/.netlify/functions/hello-world').then(
@@ -11,10 +13,24 @@ document.addEventListener('DOMContentLoaded', () => {
     responseText.innerText = JSON.stringify(response);
   });
 
-  fetchPokedexBtn.addEventListener('click', async () => {
-    const response = await fetch('/.netlify/functions/pokedex').then(
-      (response) => response.json()
-    );
+  fetchKantoBtn.addEventListener('click', async () => {
+    const response = await fetch('/.netlify/functions/pokedex', {
+      method: 'POST',
+      body: JSON.stringify({
+        region: 'kanto',
+      }),
+    }).then((response) => response.json());
+
+    responseText.innerText = JSON.stringify(response);
+  });
+
+  fetchHoennBtn.addEventListener('click', async () => {
+    const response = await fetch('/.netlify/functions/pokedex', {
+      method: 'POST',
+      body: JSON.stringify({
+        region: 'hoenn',
+      }),
+    }).then((response) => response.json());
 
     responseText.innerText = JSON.stringify(response);
   });
